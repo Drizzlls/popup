70f59ecdfe0d28ec6f78f1370f3eedc8507045a0
Ruslan <41925369+Drizzlls@users.noreply.github.com>
2020-10-13 21:37:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-let more = document.querySelector('.more'),
-    overlay = document.querySelector('.overlay'),
-    close = document.querySelector('.popup-close');
+// модальное окно
+document.addEventListener('DOMContentLoaded', function() {
+    !function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
 
-    more.addEventListener('click', function(){
-        overlay.style.display = 'block';
-        this.classList.add('more-splash');
-        document.body.style.overflow = 'hidden';
-    });
+    let button = document.querySelectorAll('.button'),
+        close = document.querySelectorAll('.close'),
+        modal = document.querySelector('.modal');
 
-    close.addEventListener('click', function(){
-        overlay.style.display = 'none';
-        more.classList.remove('more-splash');
-        document.body.style.overflow = '';
+
+
+    button.forEach(function(element){
+        element.addEventListener('click', function(e){
+            element.preventDefault;
+            modal.classList.add('hide');
+            document.body.style = "overflow:hidden";
+
+        });
+
     });
+    
+    close.forEach(function(item){
+        item.addEventListener('click', function(e) {
+            modal.classList.remove('hide');
+            document.body.style = "overflow:visible";
+        });
+     });
+
+});
 
